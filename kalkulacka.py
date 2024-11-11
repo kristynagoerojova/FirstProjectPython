6d0404a1cfa4d231c8028c705eb18f1b5620fff0
kristyna goerojo <92547229+kristynagoe@users.noreply.github.com>
2024-11-11 20:54:20 +0100

    added exceptions

--- kalkulacka.py
+++ kalkulacka.py
@@ -1,27 +1,31 @@
 def spocitej_to():
     try:
         prvni_cislo = int(input("Zadej cislo: "))
-
-        operator = input("Zadej operator (+, -, *, /): ")
-
-        druhe_cislo = int(input("Zadej dalsi cislo: "))
-
-        if operator == "+":
-            return prvni_cislo + druhe_cislo
-        elif operator == "-":
-            return prvni_cislo - druhe_cislo
-        elif operator == "*":
-            return prvni_cislo * druhe_cislo
-        elif operator == "/":
-            try:
-                return prvni_cislo / druhe_cislo
-            except ZeroDivisionError:
-                print("Nelze delit nulou.")
-        else:
-            return "Zadal si nespravny operator"
-
     except ValueError:
-        return "Muzes zadat pouze cislo."
+        print("Muzes zadat pouze cislo.")
+
+    operator = input("Zadej operator (+, -, *, /): ")
+    if operator not in ["+", "-", "*", "/"]:
+        raise ValueError("Spatne zadane znamenko")
+
+    try:
+        druhe_cislo = int(input("Zadej dalsi cislo: "))
+    except ValueError:
+        print("Muzes zadat pouze cislo.")
+
+    if operator == "+":
+        return prvni_cislo + druhe_cislo
+    elif operator == "-":
+        return prvni_cislo - druhe_cislo
+    elif operator == "*":
+        return prvni_cislo * druhe_cislo
+    elif operator == "/":
+        try:
+            return prvni_cislo / druhe_cislo
+        except ZeroDivisionError:
+            print("Nelze delit nulou.")
+    else:
+        return "Zadal si nespravny operator"
 
 vysledek = spocitej_to()
 
