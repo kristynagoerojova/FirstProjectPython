a9e71ba426e359fd098e334c8e338b1ef97b80a8
kristyna goerojo <92547229+kristynagoe@users.noreply.github.com>
2024-11-11 19:40:46 +0100

    added exception for value error

--- kalkulacka.py
+++ kalkulacka.py
@@ -1,23 +1,27 @@
 def spocitej_to():
-    prvni_cislo = int(input("Zadej cislo: "))
+    try:
+        prvni_cislo = int(input("Zadej cislo: "))
 
-    operator = input("Zadej operator (+, -, *, /): ")
+        operator = input("Zadej operator (+, -, *, /): ")
 
-    druhe_cislo = int(input("Zadej dalsi cislo: "))
+        druhe_cislo = int(input("Zadej dalsi cislo: "))
 
-    if operator == "+":
-        return prvni_cislo + druhe_cislo
-    elif operator == "-":
-        return prvni_cislo - druhe_cislo
-    elif operator == "*":
-        return prvni_cislo * druhe_cislo
-    elif operator == "/":
-        try:
-            return prvni_cislo / druhe_cislo
-        except ZeroDivisionError:
-            print("Nelze delit nulou.")
-    else:
-        return "Zadal si nespravny operator"
+        if operator == "+":
+            return prvni_cislo + druhe_cislo
+        elif operator == "-":
+            return prvni_cislo - druhe_cislo
+        elif operator == "*":
+            return prvni_cislo * druhe_cislo
+        elif operator == "/":
+            try:
+                return prvni_cislo / druhe_cislo
+            except ZeroDivisionError:
+                print("Nelze delit nulou.")
+        else:
+            return "Zadal si nespravny operator"
+
+    except ValueError:
+        return "Muzes zadat pouze cislo."
 
 vysledek = spocitej_to()
 
