d646b60eaacd8e0004f280f11553ea30103af31a
kristyna goerojo <92547229+kristynagoe@users.noreply.github.com>
2024-11-13 17:48:31 +0100

    added total weight

--- tridy.py
+++ tridy.py
@@ -1,12 +1,20 @@
 class Animal:
+    total_weight = 0
+
     def __init__(self, weight, age):
         self.weight = weight
         self.age = age
+        Animal.total_weight += weight
+
     def look(self):
         print(f"The {self.age} y.o. animal weighs {self.weight} kg and looks around.")
 
     def breathe(self):
         print(f"The {self.age} y.o. animal weighs {self.weight} kg and breathes.")
+
+    @classmethod
+    def get_total_weight(cls):
+        print(f"Total weigh of animals is {cls.total_weight}")
 
 class Fish(Animal):
     def swim(self):
@@ -34,13 +42,19 @@ class Bird(Animal):
 
 
 my_dog = DomesticDog(10,8,"German boxer", "brindle" )
-my_dog.bark()
-
+# my_dog.bark()
+#
 my_bird = Bird(2, 15)
-my_bird.fly()
+# my_bird.fly()
 
 generic_animal = Animal(150, 30)
-generic_animal.breathe()
+# generic_animal.breathe()
+
+zvire = Animal(250, 30)
+
+Animal.get_total_weight()
+
+
 
 
 
