54180abb08f8dcc353708a033055592fb8bba689
kristyna goerojo <92547229+kristynagoe@users.noreply.github.com>
2024-12-02 19:43:55 +0100

    task 13 corrected

--- threads_task.py
+++ threads_task.py
@@ -19,32 +19,36 @@ import threading
 
 
 class ThreadWithReturnValue(threading.Thread):
-    def __init__(self, target):
+    def __init__(self, target, args=(), kwargs=None):
+        if kwargs is None:
+            kwargs = {}
         self.target = target
+        self.args = args
+        self.kwargs = kwargs
         super().__init__()
 
     def run(self):
-        self.result = self.target()
+        self.result = self.target(*self.args, **self.kwargs)
 
     def join(self, timeout=None):
         super().join(timeout)
         return self.result
 
-def scitani():
+def scitani(cisla):
     soucet = 0
-    for cislo in range(1000000):  # Loop from 0 to 999999
+    for cislo in range(cisla):  # Loop from 0 to 999999
         soucet += cislo
     return soucet
 
-def nasobeni():
+def nasobeni(cisla):
     nasobek = 1
-    for cislo in range(1, 100):
+    for cislo in range(1, cisla):
         nasobek *= cislo
     return nasobek
 
 if __name__ == "__main__":
-    vysledek_scitani = ThreadWithReturnValue(target=scitani)
-    vysledek_nasobeni = ThreadWithReturnValue(target=nasobeni)
+    vysledek_scitani = ThreadWithReturnValue(target=scitani, args=(1000000,))
+    vysledek_nasobeni = ThreadWithReturnValue(target=nasobeni, args=(100,))
 
     vysledek_scitani.start()
     vysledek_nasobeni.start()
